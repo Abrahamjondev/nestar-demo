d16a793ef99621f938f0a8a85321d1b5042d834c
Abrahamjondev <ibrohimjon0425@gmail.com>
2026-04-30 13:52:39 +0500

    feat: integration header and footer via layouts

--- pages/property/detail.tsx
+++ pages/property/detail.tsx
@@ -1,11 +1,10 @@
 import withLayoutBasic from "@/libs/components/layout/LayoutBasic";
 import { Container, Stack } from "@mui/material";
-import { NextPage } from "next";
 
-const PropertyList: NextPage = () => {
+const PropertyDetail = () => {
   console.log("PROPERTYLIST COMPONENT - PAGES ROUTER");
 
   return <Container>PROPERTY LIST</Container>;
 };
 
-export default withLayoutBasic(PropertyList);
+export default withLayoutBasic(PropertyDetail);
