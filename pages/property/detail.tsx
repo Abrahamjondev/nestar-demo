119b33bf378779dd66386eb5acbe36bb0f362d5a
Abrahamjondev <ibrohimjon0425@gmail.com>
2026-05-02 12:24:33 +0500

    feat: publish homepage screen component

--- pages/property/detail.tsx
+++ pages/property/detail.tsx
@@ -1,9 +1,7 @@
 import withLayoutBasic from "@/libs/components/layout/LayoutBasic";
-import { Container, Stack } from "@mui/material";
+import { Container } from "@mui/material";
 
 const PropertyDetail = () => {
-  console.log("PROPERTYLIST COMPONENT - PAGES ROUTER");
-
   return <Container>PROPERTY LIST</Container>;
 };
 
