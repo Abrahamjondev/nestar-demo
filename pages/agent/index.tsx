4cd5512932d3fa6d96fa5335e5fb81809462e0fe
Abrahamjondev <ibrohimjon0425@gmail.com>
2026-05-07 08:55:03 +0500

    feat: develop mobile version

--- pages/agent/index.tsx
+++ pages/agent/index.tsx
@@ -1,13 +1,20 @@
 import withLayoutBasic from "@/libs/components/layout/LayoutBasic";
+import useDeviceDetect from "@/libs/hooks/useDeviceDetect";
 import { Stack } from "@mui/material";
 import { NextPage } from "next";
 
 const Agent: NextPage = () => {
-  return (
-    <div style={{ margin: "20px 0" }}>
-      <Stack className="container">AGENT</Stack>
-    </div>
-  );
+  const device = useDeviceDetect();
+
+  if (device === "mobile") {
+    return <Stack>AGENT LIST MOBILE</Stack>;
+  } else {
+    return (
+      <div style={{ margin: "20px 0" }}>
+        <Stack className="container">AGENTS LIST</Stack>
+      </div>
+    );
+  }
 };
 
 export default withLayoutBasic(Agent);
